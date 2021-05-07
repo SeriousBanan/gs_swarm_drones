f98c958bec776bcc73def5b0ea507d2a8642e653
Eduard Lazarev <seriousbanan@gmail.com>
2021-05-07 15:46:47 +0300

    Remove counter of missed frames in recognise.py

--- scripts/recognise.py
+++ scripts/recognise.py
@@ -11,8 +11,6 @@ from sensor_msgs.msg import Image
 from gs_swarm_drones.srv import Recognise, RecogniseResponse  # type: ignore
 # pylint: enable=import-error, no-name-in-module
 
-import os
-
 try:
     from .detector.detector import Detector
 except ImportError:
@@ -21,27 +19,20 @@ except ImportError:
 detector = Detector(0.5)
 img = []
 bridge = CvBridge()
-count_missed = 0
 
 
 def img_callback(data):
     """Get image from camera."""
+
     global img
-    global count_missed
     try:
         img = bridge.imgmsg_to_cv2(data)
-        count_missed = 0
     except BaseException:
         img = []
-        count_missed += 1
-    with open(f"{os.getenv('PKG_PATH')}/___missed_counter.txt", "a") as f:
-        print(count_missed, file=f)
 
 
 def detect_tank(_):
     """Detect tank using camera."""
-    with open(f"{os.getenv('PKG_PATH')}/___type_of_image.txt", "a") as f:
-        print(type(img), file=f)
 
     flag, path = detector.detect(img)
     return RecogniseResponse(flag, path)
