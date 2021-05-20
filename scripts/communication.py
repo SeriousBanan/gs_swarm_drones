d6fe568bbfb8cad5fc8040171d06129aacc09ea0
Eduard Lazarev <seriousbanan@gmail.com>
2021-05-20 20:13:33 +0300

    Add catching exception in listener

--- scripts/communication.py
+++ scripts/communication.py
@@ -123,54 +123,62 @@ def listening() -> NoReturn:
     server_sock.listen(10)
 
     while True:
-        # Waiting for connection.
-        sock, _ = server_sock.accept()
-        message_info = {}  # type: ignore
-        message = b""
-        finished = False
+        try:
+            # Waiting for connection.
+            sock, _ = server_sock.accept()
+            message_info = {}  # type: ignore
+            message = b""
+            finished = False
 
-        with sock:
-            # Receiving data.
-            while True:
-                data = sock.recv(BUFFER_SIZE)
+            with sock:
+                # Receiving data.
+                while True:
+                    data = sock.recv(BUFFER_SIZE)
 
-                # Firstly get info about message.
-                if not message_info:
-                    message_info = json.loads(data.decode())
-                    sock.sendall(b"got message info")
+                    # Firstly get info about message.
+                    if not message_info:
+                        message_info = json.loads(data.decode())
+                        sock.sendall(b"got message info")
 
-                # Close connection after receiving whole message or if connection broken.
-                elif data == b"$close$" or not data:
-                    sock.close()
-                    break
+                    # Close connection after receiving whole message or if connection broken.
+                    elif data == b"$close$" or not data:
+                        sock.close()
+                        break
 
-                elif not finished:
-                    if data[-10:] == b"$finished$":
-                        data = data[:-10]
-                        finished = True
+                    elif not finished:
+                        if data[-10:] == b"$finished$":
+                            data = data[:-10]
+                            finished = True
 
-                    message += data
+                        message += data
 
-                # If not all data received send error.
-                if finished and len(message) != message_info["size"]:
-                    sock.sendall(b"error while receiving message")
-                    message = b""
-                    finished = False
+                    # If not all data received send error.
+                    if finished and len(message) != message_info["size"]:
+                        sock.sendall(b"error while receiving message")
+                        message = b""
+                        finished = False
 
-                # Send to device message that all received.
-                elif finished:
-                    sock.sendall(b"got message")
+                    # Send to device message that all received.
+                    elif finished:
+                        sock.sendall(b"got message")
 
-        # Processing text
-        if message_info["type"] == "text":
-            responce = False
-            while not responce:
-                responce = send_received_data(message.decode()).status
+            # Processing text
+            if message_info["type"] == "text":
+                responce = False
+                while not responce:
+                    responce = send_received_data(message.decode()).status
 
-        # Processing image
-        elif message_info["type"] == "image":
-            with open(CONFIGURATION["image file path"], "wb") as image_file:
-                image_file.write(message)
+            # Processing image
+            elif message_info["type"] == "image":
+                with open(CONFIGURATION["image file path"], "wb") as image_file:
+                    image_file.write(message)
+        except socket.error:
+            server_sock.close()
+            server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_sock.bind(server_address)
+
+            # Listening to incoming connections.
+            server_sock.listen(10)
 
 
 if __name__ == '__main__':
